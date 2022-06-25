f4534bab5f07675d873f51cb61215b9c0be76016
FishWell <46333677+FishWell@users.noreply.github.com>
2022-06-26 06:23:14 +0700

    Add files via upload

---
+++ start.py
@@ -0,0 +1,12 @@
+# pylint: disable=missing-module-docstring
+
+# import mmap
+
+with open('zoids.gba','rb') as f:
+    A = f.read()
+    a = []
+    for addr in range(0x7b9c28, 0x7BE273):
+        a.append(A[addr])
+        if len(a)==8:
+            print('{:04x}:{:02x} {:02x} {:02x} {:02x} {:02x} {:02x} {:02x} {:02x}'.format(addr,a[0],a[1],a[2],a[3],a[4],a[5],a[6],a[7]))
+            a = []
